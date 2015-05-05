fbf862ef05ca27fe09952a83e59eaad713408a08
crissdev <criss.trifan@gmail.com>
2015-05-05 23:59:24 +0300

    chore(*): update dependencies

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ System.config({
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.1.13",
-    "babel-runtime": "npm:babel-runtime@5.1.13",
-    "core-js": "npm:core-js@0.9.4",
+    "babel": "npm:babel-core@5.2.15",
+    "babel-runtime": "npm:babel-runtime@5.2.15",
+    "core-js": "npm:core-js@0.9.6",
     "crossroads": "npm:crossroads@0.12.0",
     "css": "github:systemjs/plugin-css@0.1.10",
     "hasher": "npm:hasher@1.2.0",
@@ -35,7 +35,7 @@ System.config({
       "assert": "npm:assert@1.3.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.2.1"
+      "buffer": "npm:buffer@3.2.2"
     },
     "github:jspm/nodelibs-events@0.1.0": {
       "events-browserify": "npm:events-browserify@0.0.1"
@@ -86,9 +86,9 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:buffer@3.2.1": {
+    "npm:buffer@3.2.2": {
       "base64-js": "npm:base64-js@0.0.8",
-      "ieee754": "npm:ieee754@1.1.4",
+      "ieee754": "npm:ieee754@1.1.5",
       "is-array": "npm:is-array@1.0.1"
     },
     "npm:clean-css@3.1.9": {
@@ -110,10 +110,10 @@ System.config({
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@0.8.4": {
+    "npm:core-js@0.9.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@0.9.4": {
+    "npm:core-js@0.9.6": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:core-util-is@1.0.1": {
@@ -140,9 +140,6 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:jquery@2.1.3": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:jquery@2.1.4": {
       "process": "github:jspm/nodelibs-process@0.1.1"
